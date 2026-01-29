4fb70b01e11c535ca5b9b3a1d72f8106b229c653
Jeferson Szalanski <jefersonszalanski@gmail.com>
2026-01-29 11:55:02 -0300

    Add vite-plugin-singlefile to Vite config

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Mantenha o seu plugin atual (pode ser vue também)
-import { viteSingleFile } from "vite-plugin-singlefile" // <--- 1. ADICIONE ESTA LINHA
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(), // Mantenha seus plugins atuais
-    viteSingleFile() // <--- 2. ADICIONE ESTA LINHA DENTRO DOS PLUGINS
+    react(),
+    viteSingleFile()
   ],
   build: {
-    // Isso garante que assets pequenos também sejam inline (opcional, mas recomendado)
+    // Isso garante que não sobre nenhum arquivo externo
     assetsInlineLimit: 100000000, 
-  }
+    chunkSizeWarningLimit: 100000000,
+    cssCodeSplit: false,
+    rollupOptions: {
+      inlineDynamicImports: true,
+    },
+  },
 })
